a7c9102840b28dfca64ba57d32ab77ab7234ee4a
Taras Kudryavtsev <gdieho@gmail.com>
2012-07-02 18:18:31 +0300

    BatleCalculates - part 1 (Chance to Hit metod)

---
+++ WindowsFormsApplication1/Calculator/BatleCalculates.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WindowsFormsApplication1.MonsterType;
+
+namespace WindowsFormsApplication1.Calculator
+{
+    class BatleCalculates
+    {
+
+        public bool ChanceToHit(ILive first)
+        {
+            Random rand = new Random();
+            double chanceToHit = rand.NextDouble();
+            switch (first.Type)
+            {
+                case "Defender": if (chanceToHit > 0.8)
+                        return false; break;
+                case "Wizard": break;
+                case "DD": if (chanceToHit > 0.9)
+                        return false; break;
+                case "Monster1": if (chanceToHit > 0.5)
+                        return false; break;
+                case "Monster2": if (chanceToHit > 0.6)
+                        return false; break;
+                case "Monster3": if (chanceToHit > 0.7)
+                        return false; break;
+            }
+            return true;
+        }
+    }
+}
